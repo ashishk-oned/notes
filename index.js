40c31e910cf6944d44deaeb98a2cfe90fbab6be7
ashish <ashishkag.oned@gmail.com>
2023-06-29 05:21:02 +0530

    second commit

--- index.js
+++ index.js
@@ -92,6 +92,7 @@ app.post('/api/notes', (request, response) => {
 });
 
 
-const PORT = 3001;
-app.listen(PORT);
-console.log(`Server running on port ${PORT}`);
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+  })
